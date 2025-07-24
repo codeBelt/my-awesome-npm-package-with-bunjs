b0fe74c7078547da803c96878e47ca0133c8144e
codeBelt <robertsavian@gmail.com>
2025-07-23 22:33:49 -0500

    Initial

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,3 @@ export type { NumberUtils } from "./utils/numbers/numbers.types.ts";
 
 export { stringUtils } from "./utils/strings/strings.ts";
 export type { StringFormatter } from "./utils/strings/strings.types.ts";
-
-console.log("Hello via Bun!");
